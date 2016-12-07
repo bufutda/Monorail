9c4402649025321a71bc843e958397131e096d2d
Connor Newman <cjnewman@shaw.ca>
2016-12-06 19:40:49 -0700

    fixing js

--- web/stop-find.js
+++ web/stop-find.js
@@ -13,7 +13,7 @@ function doFunction () {
         $("#prev-stop tbody").empty();
         data = data.data;
         for(var i = 0; i < data.length; i++) {
-          $("#prev-stop tbody").append("<tr><td>"+data.No+"</td><td>"+data.Location+"</td><td>"+data.rID+"</td></tr>");
+          $("#prev-stop tbody").append("<tr><td>"+data[i].No+"</td><td>"+data[i].Location+"</td><td>"+data[i].rID+"</td></tr>");
         }
       } else {
       onError();
